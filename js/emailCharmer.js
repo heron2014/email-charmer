5db41d65b50949fe14d14b887e19f4d2a2d883cb
Anita <a.nita@hotmail.co.uk>
2016-12-23 19:46:06 +0000

    refactor: removes previous logic, on extension click populate bodyMessage #3

--- js/emailCharmer.js
+++ js/emailCharmer.js
@@ -1,13 +1,16 @@
-(function () {
-  window.addEventListener('DOMContentLoaded', () => {
-    const bodyMessage = document.getElementsByClassName('Am Al editable LW-avf')[0].innerText.split(' ');
-     bodyMessage.map((word) => {
-       bodyMessage.addEventListener('click', () => {
-        console.log('word', word));
-        chrome.runtime.sendMessage({ word: word }, function(response) {
-          console.log('background response: ', response.status, response);
-        });
-      });
-    });
-  });
-}());
+console.log('emailCharmer');
+
+// const bodyMessage = document.getElementsByClassName('Am Al editable LW-avf')[0];
+// console.log(bodyMessage);
+//   bodyMessage.addEventListener('change', function () {
+//     console.log('something');
+//   })
+//  bodyMessage.map((word) => {
+//    console.log('1', word);
+//    bodyMessage.addEventListener('click', () => {
+//     console.log('word', word);
+//     chrome.runtime.sendMessage({ word: word }, function(response) {
+//       console.log('background response: ', response.status, response);
+//     });
+//   });
+// });

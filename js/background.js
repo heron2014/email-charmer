7198d17b36318e971a8f86a1c6bff160b7ec401e
Anita <a.nita@hotmail.co.uk>
2016-12-23 22:58:21 +0000

    feat: word on click is send to background, background sends response back to content_script with dummy word for now #3

--- js/background.js
+++ js/background.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log('request', request);
-    sendResponse({message: 'OK'})
+    sendResponse({ newWord: 'synonym' })
   }
 )
 
--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 console.log('script');
-const bodyMessage = document.getElementsByClassName('Am Al editable LW-avf')[0];
-console.log(bodyMessage);
+document.getElementsByClassName('Am Al editable LW-avf')[0].addEventListener('click', function(e){
+  s = window.getSelection();
+  var range = s.getRangeAt(0);
+  var node = s.anchorNode;
+
+  while (range.toString().indexOf(' ') != 0) {
+    range.setStart(node,(range.startOffset - 1));
+  }
+  range.setStart(node, range.startOffset + 1);
+  do {
+    range.setEnd(node,range.endOffset + 1);
+  } while(range.toString().indexOf(' ') == -1 && range.toString().trim() != '');
+
+  var str = range.toString().trim();
+  console.log(str);
+
+  chrome.runtime.sendMessage({ word: str }, function(response) {
+    console.log('background response: ', response.newWord, response);
+  });
+});

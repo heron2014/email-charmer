b3ffe081b8f34daa63ec70d7d53185cf96bd7ecb
Anita <a.nita@hotmail.co.uk>
2016-12-23 16:34:46 +0000

    adds bacground script and sending word from content script #3

--- js/emailCharmer.js
+++ js/emailCharmer.js
@@ -3,7 +3,10 @@
     const bodyMessage = document.getElementsByClassName('Am Al editable LW-avf')[0].innerText.split(' ');
      bodyMessage.map((word) => {
        bodyMessage.addEventListener('click', () => {
-        console.log(word));
+        console.log('word', word));
+        chrome.runtime.sendMessage({ word: word }, function(response) {
+          console.log('background response: ', response.status, response);
+        });
       });
     });
   });

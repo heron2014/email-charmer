46b4efb0c92246cb9afc21a63c41175a5ae1af67
Anita <a.nita@hotmail.co.uk>
2016-12-24 15:46:41 +0000

    feat: request to WordsApi is succesfull, sends back new word works #2

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
-console.log('script');
-document.getElementsByClassName('Am Al editable LW-avf')[0].addEventListener('click', function(e){
-  s = window.getSelection();
-  var range = s.getRangeAt(0);
-  var node = s.anchorNode;
+(function () {
+  if (document.getElementsByClassName('Am Al editable LW-avf')[0]) {
+    document.getElementsByClassName('Am Al editable LW-avf')[0].addEventListener('click', function(e){
+      s = window.getSelection();
+      var range = s.getRangeAt(0);
+      var node = s.anchorNode;
 
-  while (range.toString().indexOf(' ') != 0) {
-    range.setStart(node,(range.startOffset - 1));
+      while (range.toString().indexOf(' ') != 0) {
+        range.setStart(node,(range.startOffset - 1));
+      }
+      range.setStart(node, range.startOffset + 1);
+      do {
+        range.setEnd(node,range.endOffset + 1);
+      } while(range.toString().indexOf(' ') == -1 && range.toString().trim() != '');
+
+      var str = range.toString().trim();
+      console.log(str);
+
+      chrome.runtime.sendMessage({ word: str }, function(response) {
+        console.log('background response: ', response.newWord, response);
+      });
+    });
   }
-  range.setStart(node, range.startOffset + 1);
-  do {
-    range.setEnd(node,range.endOffset + 1);
-  } while(range.toString().indexOf(' ') == -1 && range.toString().trim() != '');
 
-  var str = range.toString().trim();
-  console.log(str);
-
-  chrome.runtime.sendMessage({ word: str }, function(response) {
-    console.log('background response: ', response.newWord, response);
-  });
-});
+})();
